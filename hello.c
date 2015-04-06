d4218d95f26c8e372c9d154a4864d6251aeda13a
Zimeon <severalstars@gmail.com>
2015-04-06 14:36:40 +0200

    Whatever

--- hello.c
+++ hello.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-    
+	int a=20;
 	printf("Hello world\n");
-    printf("Bye bye world");
-    printf("Zimeon was here.\n");
+	printf("Bye bye world");
+	printf("Zimeon was here.\n");
+	
     
     return 0;
 }
