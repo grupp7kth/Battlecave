cb26d42f87c88c3be81256dd3cfc16792d7f5123
Carl-Johan Dahlman <carljohan.d@hotmail.com>
2015-04-22 11:33:09 +0200

    0

--- chattserverfunction.c
+++ chattserverfunction.c
@@ -1,1 +1,17 @@
+#include "foo.h"  /* Include the header (not strictly necessary here) */
 
+SDL_TreadFunction *chattserverfunction(int currentclientid)    /* Function definition */
+{
+    int clientid;
+    char UDPtext[]; char TCPtext[];
+    
+    clientid = currentclientid;
+    
+    while(){
+        
+        SDL_TCP_Recv();
+    }
+    
+    
+    
+}
