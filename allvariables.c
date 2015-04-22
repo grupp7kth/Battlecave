8845f74b975abf67eb4f7f8fff9402bc2e428b11
marthel <marthel@kth.se>
2015-04-22 13:59:20 +0200

    0

--- allvariables.c
+++ allvariables.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
+#include "allvariables.h"
 
+#define MAX_CLIENTS 10
+
+
+SDL_Thread *client_thread[MAX_CLIENTS];
+SDLNet_SocketSet socketSet;
+TCPsocket clientTCPsocket[MAX_CLIENTS];
+UDPsocket clientUDPsocket[MAX_CLIENTS];
+int activeClients;
--- allvariables.h
+++ allvariables.h
@@ -1,1 +1,10 @@
+#ifndef ALVARIABLES_H_   /* Include guard */
+#define ALVARIABLES_H_
 
+extern SDL_Thread *client_thread[MAX_CLIENTS];
+extern SDLNet_SocketSet socketSet;
+extern TCPsocket clientTCPsocket[MAX_CLIENTS];
+extern UDPsocket clientUDPsocket[MAX_CLIENTS];
+extern int activeClients;
+
+#endif // FOO_H_
